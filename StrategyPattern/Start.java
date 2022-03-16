e672389670b908e8aad34ad90608c46e701fe00b
Aliasz1 <mzakwanas@protonmail.com>
2022-03-16 22:39:13 +0800

    1.2

--- StrategyPattern/Start.java
+++ StrategyPattern/Start.java
@@ -3,6 +3,9 @@ public class Start{
     public static void main(String[] args){
 
         Human user = new Human();
-        Context context = new Context(new HumanInterface(user,1,2,3));
+        Context context = new Context(new move());
+        user = context.goMove(user, 1, 2, 3);
+        user = context.goMove(user, 10, 22, 35);
+        user = context.goMove(user, -1, 2, 0);
     }
 }
